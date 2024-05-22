5ff2e697a77595054382b8d7643cffc33e4fbc9e
volamthienphu <phuvo@hitek.com.vn>
2024-05-22 17:41:38 +0700

    design a a text field TODO input and ADD button

--- app/src/main/java/com/thienphu/todo_list_project/MainActivity.kt
+++ app/src/main/java/com/thienphu/todo_list_project/MainActivity.kt
@@ -1,15 +1,43 @@
 package com.thienphu.todo_list_project
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.thienphu.todo_list_project.ui.theme.TODO_List_ProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +50,69 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   MainPage()
+                    MainPage()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainPage() {
+    var toDoName by remember {
+        mutableStateOf("")
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp), verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = toDoName, onValueChange = { toDoName = it },
+                label = { Text(text = "Enter TODO") },
+                colors = TextFieldDefaults.textFieldColors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedLabelColor = Color.Green,
+                    unfocusedLabelColor = Color.White,
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    unfocusedTextColor = Color.White,
+                    focusedTextColor = Color.White
+                ),
+                shape = RoundedCornerShape(5.dp),
+                modifier = Modifier
+                    .border(1.dp, Color.Black, RoundedCornerShape(5.dp))
+                    .weight(7F)
+                    .height(60.dp),
+                textStyle = TextStyle(textAlign = TextAlign.Center),
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .weight(3F)
+                    .height(60.dp),
+                colors = ButtonDefaults.buttonColors(
+                    colorResource(id = R.color.green),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(5.dp),
+                border = BorderStroke(1.dp,Color.Black)
+            ) {
+                Text(text = "ADD", fontSize = 20.sp)
+            }
+
+        }
+    }
 
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    TODO_List_ProjectTheme {
-//        Greeting("Android")
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TODO_List_ProjectTheme {
+        MainPage()
+    }
+}
